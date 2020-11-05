fdd7335cc1a2aaab551f375c7dea4c2d13f24c04
Liam Gleeson <43632442+Ljgleeson@users.noreply.github.com>
2020-11-04 21:32:00 -0800

    Server

---
+++ prog1_server.c
@@ -0,0 +1,136 @@
+/* demo_server.c - code for example server program that uses TCP */
+
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define QLEN 6 /* size of request queue */
+
+/*------------------------------------------------------------------------
+* Program: demo_server
+*
+* Purpose: allocate a socket and then repeatedly execute the following:
+* (1) wait for the next connection from a client
+* (2) fork and respond to client guesses
+* (3) close the connection
+* (4) go back to step (1)
+*
+* Syntax: ./demo_server server_port secret_number
+*
+* server_port - protocol port number to use
+* secret_number - number for clients to guess.
+*------------------------------------------------------------------------
+*/
+
+int main(int argc, char **argv) {
+	struct protoent *ptrp; /* pointer to a protocol table entry */
+	struct sockaddr_in sad; /* structure to hold server's address */
+	struct sockaddr_in cad; /* structure to hold client's address */
+	int sd, sd2; /* socket descriptors */
+	int port; /* protocol port number */
+	int alen; /* length of address */
+	int optval = 1; /* boolean value when we set socket option */
+	uint32_t secret_number; /* number for clients to guess */
+
+	if( argc != 3 ) {
+		fprintf(stderr,"Error: Wrong number of arguments\n");
+		fprintf(stderr,"usage:\n");
+		fprintf(stderr,"./server server_port secret_number\n");
+		exit(EXIT_FAILURE);
+	}
+
+	secret_number = strtoul(argv[2], 0, 10); 
+
+	memset((char *)&sad,0,sizeof(sad)); /* clear sockaddr structure */
+	sad.sin_family = AF_INET; /* set family to Internet */
+	sad.sin_addr.s_addr = INADDR_ANY; /* set the local IP address */
+
+	port = atoi(argv[1]); /* convert argument to binary */
+	if (port > 0) { /* test for illegal value */
+		sad.sin_port = htons((u_short)port);
+	} else { /* print error message and exit */
+		fprintf(stderr,"Error: Bad port number %s\n",argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	/* Map TCP transport protocol name to protocol number */
+	if ( ((long int)(ptrp = getprotobyname("tcp"))) == 0) {
+		fprintf(stderr, "Error: Cannot map \"tcp\" to protocol number");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Create a socket */
+	sd = socket(PF_INET, SOCK_STREAM, ptrp->p_proto);
+	if (sd < 0) {
+		fprintf(stderr, "Error: Socket creation failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Allow reuse of port - avoid "Bind failed" issues */
+	if( setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0 ) {
+		fprintf(stderr, "Error Setting socket option failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Bind a local address to the socket */
+	if (bind(sd, (struct sockaddr *)&sad, sizeof(sad)) < 0) {
+		fprintf(stderr,"Error: Bind failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Specify size of request queue */
+	if (listen(sd, QLEN) < 0) {
+		fprintf(stderr,"Error: Listen failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Main server loop - accept and handle requests */
+	while (1) {
+		alen = sizeof(cad);
+		if ( (sd2=accept(sd, (struct sockaddr *)&cad, &alen)) < 0) {
+			fprintf(stderr, "Error: Accept failed\n");
+			exit(EXIT_FAILURE);
+		}
+
+		fprintf(stdout, "Successfully connected new client, forking...\n");
+
+		int pid = fork();
+		if(pid == 0) {
+				/* child process */
+
+				/* TODO - Add your code below. */
+
+			while(1){
+				char buf[1000];
+				int value;
+				int n = recv(sd2, buf, sizeof(buf),0); //receives the input from client
+
+				if(atoi(buf) == atoi(argv[2])){
+					value = 0;
+					send(sd2,&value,sizeof(value),0);
+					break;
+				}else if (atoi(buf) > atoi(argv[2])){
+					value = 1; //too high 
+					send(sd2,&value,sizeof(value),0);
+				}else{
+					value = -1; //too low
+					send(sd2,&value,sizeof(value),0);
+				}
+
+			}
+				/* END TODO */
+				
+			close(sd2);
+			exit(EXIT_SUCCESS);
+			
+		} else {
+			/* parent process */
+			close(sd2);
+		}
+	}
+}
